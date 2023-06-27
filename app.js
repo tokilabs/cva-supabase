cb4bd3fa82c29d2ecb6529236b14f56e39c70c19
Gabriela Sanches <gabi@toki.life>
2023-06-27 17:30:01 -0300

    chore: postgres installed and configuration for db

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://tjnagdjelhjyqalistir.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-sbp_e4e6958da4eb45d63e89581a336672b0177cfa03
+const supabaseUrl = "tjnagdjelhjyqalistir.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
